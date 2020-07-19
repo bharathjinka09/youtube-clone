09347ae3fa7924fbfa449caf58972648be2fab8e
bharathjinka09 <bharathjinka09@gmail.com>
2020-07-19 12:12:25 +0530

    Updates

---
+++ precache-manifest.de497c0728c3dcf8bc83f801b9ef356a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49012ce4af2fd7c570fc0ee7e5237701",
+    "url": "/youtube-clone/index.html"
+  },
+  {
+    "revision": "a73eeb403ce5e83afa10",
+    "url": "/youtube-clone/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "359536f182648b12b230",
+    "url": "/youtube-clone/static/css/main.5f587e23.chunk.css"
+  },
+  {
+    "revision": "a73eeb403ce5e83afa10",
+    "url": "/youtube-clone/static/js/2.cf7f8d84.chunk.js"
+  },
+  {
+    "revision": "be9831207da3d10befcd05d1ebb51a3a",
+    "url": "/youtube-clone/static/js/2.cf7f8d84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "359536f182648b12b230",
+    "url": "/youtube-clone/static/js/main.c0b14437.chunk.js"
+  },
+  {
+    "revision": "844d4cfab7860bee6e8b",
+    "url": "/youtube-clone/static/js/runtime-main.c0a9cebd.js"
+  }
+]);
